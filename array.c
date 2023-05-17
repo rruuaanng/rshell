0460834bee85b2ccb403f838ca53e7e9c7476a24
RUANG <longjinyii@outlook.com>
2023-05-17 19:41:01 +0800

    change note

--- array.c
+++ array.c
@@ -4,6 +4,12 @@
 #include "include/array.h"
 
 int init_arr(char ***args, int size) {
+    /**
+     * @brief 初始化参数数组
+     * @param 1 参数指针
+     * @param 2 数组大小
+     * @return 是否执行
+    */
     int i = 0;
     *args = (char **)malloc(sizeof(char *) * size);
     while(i < size){
@@ -17,6 +23,13 @@ int init_arr(char ***args, int size) {
 
 
 int cls_arr(char ***args,int size,int *arg_size){
+    /**
+     * @brief 释放参数指针空间
+     * @param 1 参数指针
+     * @param 2 数组大小
+     * @param 3 参数个数
+     * @return 是否执行
+    */
     if(args == NULL) return 0;
     for(int i = 0;i < size;i++){
         free((*args)[i]);

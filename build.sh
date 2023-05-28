79f6e27d8ecc613068a8e2599f9abf998a3f62fc
RUANG <108215543+rruuaanng@users.noreply.github.com>
2023-05-28 20:44:45 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
+GCC = "gcc"
+ARGS = "-Wall -c"
 if (($# == 0)); then
-    gcc -Wall -c ./parser.c -std=c99
-    gcc -Wall -c ./reader.c -std=c99
-    gcc -Wall -c ./array.c -std=c99
-    gcc ./*.o ./sh.c -o ./sh.out -g -std=c99
-    rm -f ./*.o
-else
-    gcc -Wall -c ./parser.c -std=c99
-    gcc -Wall -c ./reader.c -std=c99
-    gcc -Wall -c ./array.c -std=c99
-    gcc ./*.o ./sh.c -o ./sh.out -g -D TEST -std=c99
+    $(GCC) $(ARGS) ./parser.c -std=c99
+    $(GCC) $(ARGS) ./reader.c -std=c99
+    $(GCC) $(ARGS) ./array.c -std=c99
+    $(GCC) ./*.o ./sh.c -o ./sh.out -g -std=c99
     rm -f ./*.o
 fi
